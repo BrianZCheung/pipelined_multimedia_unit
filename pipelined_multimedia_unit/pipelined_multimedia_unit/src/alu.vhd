b849e88cb1ee818d642db8527ddc3f6c16c048e2
BrianZYCheung <32800609+BrianZYCheung@users.noreply.github.com>
2021-10-17 17:32:48 -0400

    Alu li r3 (#2)
    * initial commit and framework of alu
    
    * adds input and output to alu
    
    * manual alu tb
    
    * 4.2 000, Signed Integer Multiply-Add Low with Saturation instruction
    
    * fixes Signed Integer Multiply-Add Low with Saturation multiplier
    
    * completes Signed Integer Multiply-Add Low and High with Saturation instruction
    
    * completes Signed Integer Multiply-Subtract Low and High with Saturation instruction
    
    * completes Signed Long Multiply-add Low and High with Saturation instruction
    
    * correct the addition and subtraction of rs1 to be the full 32 or 62 bit field for the respective alu_r3 instructions
    
    * completes Signed Long Multiply-subtract Low and High with Saturation instruction and corrects the saturation of the signed long instruction
    
    * corrects associated long instructions lower and upper bound saturate conditions
    
    * completes Load Immediate instruction

---
+++ pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu.vhd
@@ -0,0 +1,129 @@
+--
+-- Entity Name : alu
+-- Entity Description: 
+-- Architecture Name : alu_arch
+-- Description :	   
+-- 
+-- Authored by : Brian Cheung and Ryuichi Lin
+--  
+library ieee;
+use ieee.std_logic_1164.all;  
+use ieee.numeric_std.all;
+
+entity alu is
+	port(
+	instruction_in : in std_logic_vector(24 downto 0);-- instruction being executed		 
+	rs_1 : in std_logic_vector(127 downto 0);   
+	rs_2 : in std_logic_vector(127 downto 0);
+	rs_3 : in std_logic_vector(127 downto 0);
+	rd : out std_logic_vector(127 downto 0)
+	);
+end alu;
+
+architecture alu_arch of alu is	 	
+
+begin
+	
+	li: process(instruction_in(24))				   
+	variable result: std_logic_vector(127 downto 0);
+	begin 
+		if(instruction_in(24) = '0') then	  	
+			result(((16*to_integer(unsigned(instruction_in(23 downto 21))))+15) downto (16*to_integer(unsigned(instruction_in(23 downto 21))))) := instruction_in(20 downto 5);	
+		end if;	  
+		rd <= result;
+	end process li;
+	
+	r3: process(instruction_in(24 downto 23)) 
+	variable longMin: integer := -(2**63);
+	variable longMax: integer := (2**63) - 1;	 
+	variable intMin: integer := -(2**31); 
+	variable intMax: integer := (2**31) - 1;
+	variable result: std_logic_vector(127 downto 0);
+	
+	begin  
+		if(instruction_in(24 downto 23) = "10") then
+			if(instruction_in(22 downto 20) = "000") then
+				for i in 0 to 3 loop
+					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+15 downto 32*i)))*to_integer( signed(rs_2((32*i)+15 downto 32*i))) ) + to_integer( signed(rs_1((32*i)+31 downto (32*i)))) ),32));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(intMax,128));
+				end if;
+			elsif(instruction_in(22 downto 20) = "001") then	 
+				for i in 0 to 3 loop
+					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+31 downto (32*i)+16)))*to_integer( signed(rs_2((32*i)+31 downto (32*i)+16))) ) + to_integer( signed(rs_1((32*i)+31 downto (32*i)))) ),32));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(intMax,128));
+				end if;	
+			elsif(instruction_in(22 downto 20) = "010") then   
+				for i in 0 to 3 loop
+					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+15 downto 32*i)))*to_integer( signed(rs_2((32*i)+15 downto 32*i))) ) - to_integer( signed(rs_1((32*i)+31 downto (32*i)))) ),32));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(intMax,128));
+				end if;
+			elsif(instruction_in(22 downto 20) = "011") then  
+				for i in 0 to 3 loop
+					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+31 downto (32*i)+16)))*to_integer( signed(rs_2((32*i)+31 downto (32*i)+16))) ) - to_integer( signed(rs_1((32*i)+31 downto (32*i)))) ),32));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(intMax,128));
+				end if;	
+			elsif(instruction_in(22 downto 20) = "100") then		   
+				for i in 0 to 1 loop
+					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+31 downto 64*i)))*to_integer( signed(rs_2((64*i)+31 downto 64*i))) ) + to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
+				end loop;	 
+				if(to_integer(signed(result)) < longMin) then
+					result := std_logic_vector(to_signed(longMin,128));
+				elsif(to_integer(signed(result)) > longMax) then
+					result := std_logic_vector(to_signed(longMax,128));
+				end if;
+			elsif(instruction_in(22 downto 20) = "101") then	   
+				for i in 0 to 1 loop
+					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+63 downto (64*i)+32)))*to_integer( signed(rs_2((64*i)+63 downto (64*i)+32))) ) + to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
+				end loop;	 
+				if(to_integer(signed(result)) < longMin) then
+					result := std_logic_vector(to_signed(longMin,128));
+				elsif(to_integer(signed(result)) > longMax) then
+					result := std_logic_vector(to_signed(longMax,128));
+				end if;
+			elsif(instruction_in(22 downto 20) = "110") then		  
+				for i in 0 to 1 loop
+					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+31 downto 64*i)))*to_integer( signed(rs_2((64*i)+31 downto 64*i))) ) - to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
+				end loop;	 
+				if(to_integer(signed(result)) < longMin) then
+					result := std_logic_vector(to_signed(longMin,128));
+				elsif(to_integer(signed(result)) > longMax) then
+					result := std_logic_vector(to_signed(longMax,128));
+				end if;
+			elsif(instruction_in(22 downto 20) = "111") then
+				for i in 0 to 1 loop
+					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+63 downto (64*i)+32)))*to_integer( signed(rs_2((64*i)+63 downto (64*i)+32))) ) - to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
+				end loop;	 
+				if(to_integer(signed(result)) < longMin) then
+					result := std_logic_vector(to_signed(longMin,128));
+				elsif(to_integer(signed(result)) > longMax) then
+					result := std_logic_vector(to_signed(longMax,128));
+				end if;
+			end if;
+		end if;		 
+		rd <= result;
+	end process r3;
+	
+	r4: process(instruction_in(24 downto 23))
+	begin
+		if(instruction_in(24 downto 23) = "11") then
+			
+		end if;
+	end process r4;
+	
+end alu_arch;
