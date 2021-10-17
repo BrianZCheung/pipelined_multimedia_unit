b4610d8e31b43d5ad4f7aed2d553008d80f8ea29
BrianZYCheung <brian.cheung1218@gmail.com>
2021-10-17 16:45:47 -0400

    completes Signed Long Multiply-subtract Low and High with Saturation instruction and corrects the saturation of the signed long instruction

--- pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu.vhd
+++ pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu.vhd
@@ -42,7 +42,7 @@ begin
 		if(instruction_in(24 downto 23) = "10") then
 			if(instruction_in(22 downto 20) = "000") then
 				for i in 0 to 3 loop
-					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+15 downto 32*i)))*to_integer( signed(rs_2((32*i)+15 downto 32*i))) ) + to_integer( signed(rs_1((32*i)+31 downto 32*i))) ),32));
+					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+15 downto 32*i)))*to_integer( signed(rs_2((32*i)+15 downto 32*i))) ) + to_integer( signed(rs_1((32*i)+31 downto (32*i)))) ),32));
 				end loop;	 
 				if(to_integer(signed(result)) < intMin) then
 					result := std_logic_vector(to_signed(intMin,128));
@@ -72,32 +72,46 @@ begin
 					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+31 downto (32*i)+16)))*to_integer( signed(rs_2((32*i)+31 downto (32*i)+16))) ) - to_integer( signed(rs_1((32*i)+31 downto (32*i)))) ),32));
 				end loop;	 
 				if(to_integer(signed(result)) < intMin) then
-					result := std_logic_vector(to_signed(intMin,128));
+					result := std_logic_vector(to_signed(longMin,128));
 				elsif(to_integer(signed(result)) > intMax) then
-					result := std_logic_vector(to_signed(intMax,128));
+					result := std_logic_vector(to_signed(longMax,128));
 				end if;	
 			elsif(instruction_in(22 downto 20) = "100") then		   
 				for i in 0 to 1 loop
 					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+31 downto 64*i)))*to_integer( signed(rs_2((64*i)+31 downto 64*i))) ) + to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
 				end loop;	 
 				if(to_integer(signed(result)) < intMin) then
-					result := std_logic_vector(to_signed(intMin,128));
+					result := std_logic_vector(to_signed(longMin,128));
 				elsif(to_integer(signed(result)) > intMax) then
-					result := std_logic_vector(to_signed(intMax,128));
+					result := std_logic_vector(to_signed(longMax,128));
 				end if;
 			elsif(instruction_in(22 downto 20) = "101") then	   
 				for i in 0 to 1 loop
 					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+63 downto (64*i)+32)))*to_integer( signed(rs_2((64*i)+63 downto (64*i)+32))) ) + to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
 				end loop;	 
 				if(to_integer(signed(result)) < intMin) then
-					result := std_logic_vector(to_signed(intMin,128));
+					result := std_logic_vector(to_signed(longMin,128));
 				elsif(to_integer(signed(result)) > intMax) then
-					result := std_logic_vector(to_signed(intMax,128));
+					result := std_logic_vector(to_signed(longMax,128));
 				end if;
 			elsif(instruction_in(22 downto 20) = "110") then		  
-				
+				for i in 0 to 1 loop
+					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+31 downto 64*i)))*to_integer( signed(rs_2((64*i)+31 downto 64*i))) ) - to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(longMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(longMax,128));
+				end if;
 			elsif(instruction_in(22 downto 20) = "111") then
-				
+				for i in 0 to 1 loop
+					result(((64*i)+63) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((64*i)+63 downto (64*i)+32)))*to_integer( signed(rs_2((64*i)+63 downto (64*i)+32))) ) - to_integer( signed(rs_1((64*i)+63 downto (64*i)))) ),64));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(longMax,128));
+				end if;
 			end if;
 		end if;		 
 		rd <= result;
