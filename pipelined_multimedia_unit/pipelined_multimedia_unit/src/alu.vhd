27817c9037c455dcf1e88bc3f1074aa1e4f5eeab
BrianZYCheung <brian.cheung1218@gmail.com>
2021-10-17 16:24:34 -0400

    completes Signed Integer Multiply-Add Low and High with Saturation instruction

--- pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu.vhd
+++ pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu.vhd
@@ -43,9 +43,21 @@ begin
 			if(instruction_in(22 downto 20) = "000") then
 				for i in 0 to 3 loop
 					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+15 downto 32*i)))*to_integer( signed(rs_2((32*i)+15 downto 32*i))) ) - to_integer( signed(rs_1((32*i)+15 downto 32*i))) ),32));
-				end loop;
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(intMax,128));
+				end if;
 			elsif(instruction_in(22 downto 20) = "001") then	 
-				
+				for i in 0 to 3 loop
+					result(((32*i)+31) downto (32*i)) := std_logic_vector(to_signed(( ( to_integer( signed(rs_3((32*i)+31 downto (32*i)+16)))*to_integer( signed(rs_2((32*i)+31 downto (32*i)+16))) ) - to_integer( signed(rs_1((32*i)+31 downto (32*i)+16))) ),32));
+				end loop;	 
+				if(to_integer(signed(result)) < intMin) then
+					result := std_logic_vector(to_signed(intMin,128));
+				elsif(to_integer(signed(result)) > intMax) then
+					result := std_logic_vector(to_signed(intMax,128));
+				end if;	
 			elsif(instruction_in(22 downto 20) = "010") then   
 				
 			elsif(instruction_in(22 downto 20) = "011") then  
