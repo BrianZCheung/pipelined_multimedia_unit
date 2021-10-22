f8eac571d7fda7ea67a65d27a2a7e0e2b233b0dc
BrianZYCheung <brian.cheung1218@gmail.com>
2021-10-22 00:19:05 -0400

    adds additional test cases to testbench for 4.2 R4 instructions

--- pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu_tb.vhd
+++ pipelined_multimedia_unit/pipelined_multimedia_unit/src/alu_tb.vhd
@@ -29,7 +29,7 @@ begin
 		rs_2 => rs_2_tb,
 		rs_3 => rs_3_tb,
 		rd => rd_tb
-	);
+		);
 	
 	stimlus: process   
 	variable max64Bit: std_logic_vector(63 downto 0) := x"7FFFFFFFFFFFFFFF";	
@@ -42,94 +42,147 @@ begin
 	variable zero64Bit: std_logic_vector(63 downto 0) := x"0000000000000000";   
 	variable zero32Bit: std_logic_vector(31 downto 0) := x"00000000";	
 	variable zero16Bit: std_logic_vector(15 downto 0) := x"0000"; 
-	begin		  
+	begin		  				
+		
+		--test first int instruction for expected values 0
+		rs_1_tb <= zero64Bit & zero32Bit & max32Bit;  
+		rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;  
+		rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;
+		instruction_in_tb <= "10" & "000" &"00000000000000000000";
+		wait for 100ns;	
+		rs_1_tb <= zero64Bit & zero32Bit & zero32Bit;  
+		rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & zero16Bit;  
+		rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & zero16Bit;
+		instruction_in_tb <= "10" & "000" &"00000000000000000000";
+		wait for 100ns;	 
+		
 		--tests for int instructions
-		for i in 0 to 3 loop   
+		for i in 0 to 3 loop	
+			--lower 16 bits non-zero
 			--+rs1 +rs2 +rs3
 			rs_1_tb <= zero64Bit & zero32Bit & max32Bit;  
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			--+rs1 +rs2 -rs3
 			rs_1_tb <= zero64Bit & zero32Bit & max32Bit;  	   
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			--+rs1 -rs2 +rs3
 			rs_1_tb <= zero64Bit & zero32Bit & max32Bit;  	   
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			--+rs1 -rs2 -rs3
 			rs_1_tb <= zero64Bit & zero32Bit & max32Bit;  	   
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 +rs2 +rs3
 			rs_1_tb <= zero64Bit & zero32Bit & min32Bit;     
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 +rs2 -rs3
 			rs_1_tb <= zero64Bit & zero32Bit & min32Bit; 	   
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 -rs2 +rs3
 			rs_1_tb <= zero64Bit & zero32Bit & min32Bit;   
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & max16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 -rs2 -rs3
 			rs_1_tb <= zero64Bit & zero32Bit & min32Bit;    
 			rs_2_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & zero16Bit & min16Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
-		end loop;	
+			
+			--upper 16 bits non-zero
+			--+rs1 +rs2 +rs3
+			rs_1_tb <= zero64Bit & max32Bit & zero32Bit;  
+			rs_2_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			--+rs1 +rs2 -rs3
+			rs_1_tb <= zero64Bit & max32Bit & zero32Bit;  	   
+			rs_2_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			--+rs1 -rs2 +rs3
+			rs_1_tb <= zero64Bit & max32Bit & zero32Bit;  	   
+			rs_2_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			--+rs1 -rs2 -rs3
+			rs_1_tb <= zero64Bit & max32Bit & zero32Bit;  	   
+			rs_2_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 +rs2 +rs3
+			rs_1_tb <= zero64Bit & min32Bit & zero32Bit;     
+			rs_2_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 +rs2 -rs3
+			rs_1_tb <= zero64Bit & min32Bit & zero32Bit; 	   
+			rs_2_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 -rs2 +rs3
+			rs_1_tb <= zero64Bit & min32Bit & zero32Bit;   
+			rs_2_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & max16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 -rs2 -rs3
+			rs_1_tb <= zero64Bit & min32Bit & zero32Bit;    
+			rs_2_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;  
+			rs_3_tb <= zero64Bit & zero32Bit & min16Bit & zero16Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+		end loop;
 		--tests for long instructions
-		for i in 5 to 8 loop   
+		for i in 4 to 7 loop   
+			--lower 64 bits non-zero
 			--+rs1 +rs2 +rs3
 			rs_1_tb <= zero64Bit & max64Bit;	   
 			rs_2_tb <= zero64Bit & zero32Bit & max32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & max32Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			--+rs1 +rs2 -rs3
 			rs_1_tb <= zero64Bit & max64Bit;	   
 			rs_2_tb <= zero64Bit & zero32Bit & max32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & min32Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			--+rs1 -rs2 +rs3
 			rs_1_tb <= zero64Bit & max64Bit;	   
 			rs_2_tb <= zero64Bit & zero32Bit & min32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & max32Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			--+rs1 -rs2 -rs3
 			rs_1_tb <= zero64Bit & max64Bit;	   
 			rs_2_tb <= zero64Bit & zero32Bit & min32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & min32Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 +rs2 +rs3
@@ -143,25 +196,73 @@ begin
 			rs_1_tb <= zero64Bit & min64Bit;	   
 			rs_2_tb <= zero64Bit & zero32Bit & max32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & min32Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 -rs2 +rs3
 			rs_1_tb <= zero64Bit & min64Bit;   
 			rs_2_tb <= zero64Bit & zero32Bit & min32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & max32Bit;
-			wait for 100ns;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 			---rs1 -rs2 -rs3
 			rs_1_tb <= zero64Bit & min64Bit;	   
 			rs_2_tb <= zero64Bit & zero32Bit & min32Bit;  
 			rs_3_tb <= zero64Bit & zero32Bit & min32Bit;
-			wait for 100ns;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	  
+			
+			--upper 64 bits non-zero
+			--+rs1 +rs2 +rs3
+			rs_1_tb <= max64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & max32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & max32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			--+rs1 +rs2 -rs3
+			rs_1_tb <= max64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & max32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & min32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			--+rs1 -rs2 +rs3
+			rs_1_tb <= max64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & min32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & max32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			--+rs1 -rs2 -rs3
+			rs_1_tb <= max64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & min32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & min32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 +rs2 +rs3
+			rs_1_tb <= min64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & max32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & max32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 +rs2 -rs3
+			rs_1_tb <= min64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & max32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & min32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 -rs2 +rs3
+			rs_1_tb <= min64Bit & zero64Bit;   
+			rs_2_tb <= zero64Bit & min32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & max32Bit & zero32Bit;
+			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
+			wait for 100ns;	
+			---rs1 -rs2 -rs3
+			rs_1_tb <= min64Bit & zero64Bit;	   
+			rs_2_tb <= zero64Bit & min32Bit & zero32Bit;  
+			rs_3_tb <= zero64Bit & min32Bit & zero32Bit;
 			instruction_in_tb <= "10" & std_logic_vector(to_unsigned(i, 3)) &"00000000000000000000";
 			wait for 100ns;	
 		end loop;
-		std.env.finish;
+		
+	std.env.finish;
 	end process;	
 	
 	
