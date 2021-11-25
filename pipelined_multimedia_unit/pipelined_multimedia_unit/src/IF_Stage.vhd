35738e8dbbb0554fb695fee1d7fbe3c6adee7d8a
BrianZYCheung <brian.cheung1218@gmail.com>
2021-11-25 12:22:32 -0500

    modifies IF_stage to load instruction from testbench and adds a testbench for IF_stage

--- pipelined_multimedia_unit/pipelined_multimedia_unit/src/IF_Stage.vhd
+++ pipelined_multimedia_unit/pipelined_multimedia_unit/src/IF_Stage.vhd
@@ -12,22 +12,38 @@ use ieee.numeric_std.all;
 
 entity IF_stage is
 	port(	 
-	pc : in std_logic_vector(31 downto 0);
+	LorF: in std_logic; --flag to load instruction buffer or fetch from instruction buffer		   
+	instrToBuffer : in std_logic_vector(24 downto 0);
+	pc_in : in std_logic_vector(31 downto 0);		  
+	pc_out : out std_logic_vector(31 downto 0);
 	instruction_out : out std_logic_vector(24 downto 0)-- instruction being executed	
 	);
-end IF_stage;
+end IF_stage;  
+
 
 architecture IF_stage_arch of IF_stage is	
 
-type instr_buffer_arr is array (63 downto 0) of std_logic_vector(24 downto 0);
-signal instr_buffer : instr_buffer_arr := (others=>(others=>'0'));
+constant MAX_BUFFER : integer := 64;
+
+type instr_buffer_arr is array ((MAX_BUFFER-1) downto 0) of std_logic_vector(24 downto 0);
+signal instr_buffer : instr_buffer_arr;
 
 begin			   
-
-	IF_module: process(pc)	  
+	
+	IB_load: process(instrToBuffer)
+	begin
+		if(LorF = '0' and to_integer(unsigned(pc_in)) < MAX_BUFFER) then
+			instr_buffer(to_integer(unsigned(pc_in))) <= instrToBuffer;
+		end if;
+	end process IB_load;
+	
+	IF_module: process(pc_in)	  
 	begin					  
 		
-		instruction_out <= instr_buffer(integer(to_integer(unsigned(pc)) / 4));
+		if(LorF = '1' and to_integer(unsigned(pc_in)) < MAX_BUFFER) then
+			instruction_out <= instr_buffer(to_integer(unsigned(pc_in)));	 
+			pc_out <= std_logic_vector(to_unsigned(to_integer(unsigned(pc_in)) + 1, 32));
+		end if;
 	
 	end process IF_module;
 
